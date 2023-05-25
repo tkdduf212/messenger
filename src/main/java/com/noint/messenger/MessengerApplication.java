ba62301b9562c43f26228ba41295d13b4b07da1c
no-int <tkdduf2122@naver.com>
2023-05-26 01:47:50 +0900

    rabbitMQ test

--- src/main/java/com/noint/messenger/MessengerApplication.java
+++ src/main/java/com/noint/messenger/MessengerApplication.java
@@ -1,11 +1,57 @@
 package com.noint.messenger;
 
+import com.noint.messenger.mq.receiver.RabbitmqReceiver;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessengerApplication {
+//    @Bean
+//    ConnectionFactory connectionFactory() {
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+//        connectionFactory.setHost("localhost");
+//        connectionFactory.setPort(5672);
+//        connectionFactory.setUsername("admin");
+//        connectionFactory.setPassword("admin");
+//        return connectionFactory;
+//    }
+    @Bean
+    Queue queue() {
+        return new Queue("test-Q", false);
+    }
 
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("test-EX");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter messageListenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames("test-Q");
+        container.setMessageListener(messageListenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(RabbitmqReceiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
     public static void main(String[] args) {
         SpringApplication.run(MessengerApplication.class, args);
     }
